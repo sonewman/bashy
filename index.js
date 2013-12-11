ab5157a9feb36dcd2dc437368273975b8deefa8c
Sam Newman <newmansam@outlook.com>
2013-12-11 21:32:09 +0000

    refactor, removed test... will be added tap test instead in near future

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-/* jshint laxcomma:true */
-module.exports = bashy;
-
-var exec = require('child_process').exec
-  // , spawn = require('child_process').spawn
-	, thru = require('thru')
-;
 
 function bashy (cmd, done) {
-	var cp, stdo, stde, out, err = [];
+  var child, stdo, stde, err = []
+    , out = new PassThrough();
 
   //  set done as noop if not fn
   done = 'function' !== typeof done ? function () {} : done;
 
   //  create child process from cmd string
-  // cp = spawn('bash');
-  // cp.stdin.end(cmd);
-  cp = exec(cmd);
+  child = exec(cmd);
   
-
   //  when the stdo(ut) through stream
   //  has finished writing to 'out'
   //  stream it will call done()
-  stdo = thru(null, done);
+  stdo = out.stdout = new PassThrough();
+  stdo.on('finish', done);
 
   //  when the stde(rr) through stream
   //  has finished writing to 'out'
   //  stream we know it has finished
-  stde = thru(null, function () {
+  stde = out.stderr = new PassThrough();
+  stde.on('finish', function () {
     //  join buffered error with line break
     //  if it is an empty string, then return
     //  undefined so as to be clear
@@ -38,28 +31,27 @@ function bashy (cmd, done) {
     //  to have not actually errored.
   });
 
-  out = thru();
-
-  out.pipe(cp.stdin);
+  //  why was i doing this again??
+  // out.pipe(child.stdin);
   
-  cp.on('exit', function (code) {
+  child.on('exit', function (code) {
     out.emit('exit', code);
   });
 
   //  bubble up errors, so they can be listen
   //  to on the return stream 
-  cp.on('error', function (a, b ,c) {
+  child.on('error', function (a, b ,c) {
     out.emit('error', a, b, c);
   });
 
   //  pipe child process stdout and stderr to
   //  coresponding through stream and then to 'out' stream
-  cp.stdout.pipe(stdo).pipe(out);
-  cp.stderr.pipe(stde).pipe(out);
+  child.stdout.pipe(stdo).pipe(out);
+  child.stderr.pipe(stde).pipe(out);
 
   //  on child process stderr buffer the
   //  error if the data read is not null
-  cp.stderr.on('data', function (chunk) {
+  child.stderr.on('data', function (chunk) {
     if (chunk !== null) err.push(chunk.toString('utf8'));
   });
 
